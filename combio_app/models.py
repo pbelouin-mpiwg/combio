bd0417a164199db3e75d555714b6b28e1e726005
pascal.belouin <pbelouin@mpiwg-berlin.mpg.de>
2023-03-28 14:23:15 +0200

    metadata scheme

--- combio_app/models.py
+++ combio_app/models.py
@@ -10,7 +10,8 @@ from django_jsonform.models.fields import JSONField
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 with open(
-    os.path.join(BASE_DIR, "combio_app") + "/static/combio_app/json/combio_metadata_scheme.json", encoding="utf-8"
+    os.path.join(BASE_DIR, "combio_app") + "/static/combio_app/metadata_scheme/combio_metadata_scheme.json",
+    encoding="utf-8",
 ) as schema_file:
     metadata_schema = json.loads(schema_file.read())
 
